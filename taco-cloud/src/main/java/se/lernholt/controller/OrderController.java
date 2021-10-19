1574bccb82a5cd034329b713f25b731fd5742a17
Kim Lernholt <lernholtkim@gmail.com>
2021-10-19 22:16:30 +0200

    LERN-000 - Fixed some stuff

--- taco-cloud/src/main/java/se/lernholt/controller/OrderController.java
+++ taco-cloud/src/main/java/se/lernholt/controller/OrderController.java
@@ -2,9 +2,11 @@ package se.lernholt.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.RequiredArgsConstructor;
+import se.lernholt.config.TacoCloudOrderConfig;
 import se.lernholt.repository.jpa.OrderRepository;
 import se.lernholt.tacos.Order;
 import se.lernholt.tacos.User;
@@ -23,11 +26,20 @@ import se.lernholt.tacos.User;
 @RequiredArgsConstructor
 public class OrderController {
 
+    private final TacoCloudOrderConfig orderConfig;
     private final OrderRepository orderRepository;
 
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        int pageSize = orderConfig.getPageSize();
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @PostMapping
